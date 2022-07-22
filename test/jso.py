149133518515aa7d65bb82d8ee044693cd590928
GoponBag <goponbag@gmail.com>
2022-07-22 20:08:52 +0530

    First Commit in Master

--- test/jso.py
+++ test/jso.py
@@ -1,7 +1,7 @@
 import json
   
 # Opening JSON file
-f = open('data44.json')
+f = open('data1.json')
 data = json.load(f)
 # data=json.dumps(data, indent=4)
 # print(data["status"],data["message"],data["data"][0]['employee_name'])
@@ -12,26 +12,37 @@ data = json.load(f)
 # ==========================================================
 # data["commits"][0]['name'],data['commits'][0]['username'],data['ref'],
 # for i in data['emp_details']:
-#     print(i)
-if data["action"] == "opened":
-    print("Pull_Request")
-    print("AUTHOR")
-    print(data["pull_request"]["base"]['label'])
-    print("TO -" )
-    print(data["pull_request"]["head"]["ref"]  )
-    print("FROM - ")
-    print(data["pull_request"]["base"]["ref"])
-    print( 'Time')
-    print(data["pull_request"]["updated_at"])
-if data["action"] == "closed":
-    print("Marge")
-    print("AUTHOR")
-    print(data["pull_request"]["base"]['label'])
-    print("TO -" )
-    print(data["pull_request"]["head"]["ref"]  )
-    print("FROM - ")
-    print(data["pull_request"]["base"]["ref"])
-    print( 'Time')
-    print(data["pull_request"]["updated_at"])
+a= "commits"
+b = "action"
+if a in data:
+    print(data["commits"][0]["id"])
+    print(data["commits"][0]['author']['username'])
+    print(data['ref'])
+    print(data['commits'][0]['timestamp'])
+elif b in data:
+    if data["action"] == "opened":
+        print("Pull_Request")
+        print("AUTHOR")
+        print(data["pull_request"]["base"]['label'])
+        print("TO -" )
+        print(data["pull_request"]["head"]["ref"]  )
+        print("FROM - ")
+        print(data["pull_request"]["base"]["ref"])
+        print( 'Time')
+        print(data["pull_request"]["updated_at"])
+        print(data["pull_request"]["id"])
+    elif data["action"] == "closed":
+        print("Marge")
+        print("AUTHOR")
+        print(data["pull_request"]["base"]['label'])
+        print("TO -" )
+        print(data["pull_request"]["head"]["ref"]  )
+        print("FROM - ")
+        print(data["pull_request"]["base"]["ref"])
+        print( 'Time')
+        print(data["pull_request"]["updated_at"])
+        print(data["pull_request"]["id"])
+    else:
+        print('nothing pull')
 else:
-    print('nothing')
+    print ("not valid")
